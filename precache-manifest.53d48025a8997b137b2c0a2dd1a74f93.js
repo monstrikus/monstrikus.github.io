324bd64ed386fdf9ea70bb6960a8ccdb603020de
monstrikus <kalugin666@yandex.ru>
2020-02-09 19:01:39 +0300

    deploy

---
+++ precache-manifest.53d48025a8997b137b2c0a2dd1a74f93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f9fa462c26a71a5558",
+    "url": "/css/app.0e88525e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a56c64daf960734a9d57c4f0044f8cec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec5b737bfc870556a002",
+    "url": "/js/about.3a7b5dcb.js"
+  },
+  {
+    "revision": "22f9fa462c26a71a5558",
+    "url": "/js/app.23029a95.js"
+  },
+  {
+    "revision": "6f93fb2e5f18372932d9",
+    "url": "/js/chunk-vendors.b42a6c66.js"
+  },
+  {
+    "revision": "f542bf4e7df044cb72378e97c4c3ad1a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
